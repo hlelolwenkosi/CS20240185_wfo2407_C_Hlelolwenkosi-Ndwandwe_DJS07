f8f30684124c200ee7ca8beec3bb5acc944a8608
Hlelolwenkosi Ndwandwe <1071hf@gmail.com>
2024-11-14 12:04:12 +0200

    created a header file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
-function App() {
-
-  return (
-    <>
-     
-    </>
-  )
+import React from "react"
+import Header from "./components/header"
+/**
+ * Challenge: Build the Header component
+ */
+export default function App() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
 }
-
-export default App
